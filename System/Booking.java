e0150ac5f6db6040a9500f1469566b502912241a
Hesham-Abdelmohsen <55280956+Hesham-Abdelmohsen@users.noreply.github.com>
2020-05-26 17:31:52 +0200

    Update Booking.java
    Hesham added refund method and used it in delete playground in playground

--- System/Booking.java
+++ System/Booking.java
@@ -1,12 +1,16 @@
 package System;
 
 import User.Player;
+import User.Team;
+
+import java.util.ArrayList;
 
 public class Booking {
 
     private Playground playground;
     private Player player;
     private Time time;
+    private ArrayList<Team.TeamMember> teamMembers;
 
     public Booking(Verifier verifier) {
         playground = new Playground(verifier);
@@ -45,6 +49,19 @@ public class Booking {
      */
     public void setTime(Time time) {
         this.time = time;
+    }
+
+    public void refund() {
+        Time currentTime = new Time();
+        currentTime.setCurrentTime();
+        if (currentTime.conflicts(getTime())) {
+            int bookingDuration = getTime().getDuration();
+            double price = playground.getPrice() * bookingDuration;
+            double currentBalance = getPlayer().getPlayerWallet().getBalance();
+            double ownerBalance = getPlayground().getOwner().getOwnerWallet().getBalance();
+            getPlayground().getOwner().getOwnerWallet().setBalance(ownerBalance - (price * bookingDuration));
+            getPlayer().getPlayerWallet().setBalance(currentBalance + (price * bookingDuration));
+        }
     }
 
     @Override
