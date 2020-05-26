b1d9d5346ec2f22c507b4356e8580ef58daedb27
AShawareb <bzsc1010@gmail.com>
2020-05-26 16:51:58 +0200

    move remove playground to playground owner

--- User/Administrator.java
+++ User/Administrator.java
@@ -15,55 +15,34 @@ public class Administrator extends Account {
         setEmail("admin@gmail.com");
         setPhoneNumber("00000000000");
     }
-    
+
     /**
      * @param playground
      */
     public void approvePlayground(Playground playground) {
         playground.setAvailability(true);
-        playground.getOwner().getPlaygrounds().add(playground);
-        playground.setPlaygroundStatus("activated");
+        playground.setPlaygroundStatus("Activated");
     }
 
     /**
      * @param playground
      */
     public void suspendPlayground(Playground playground) {
-        playground.setPlaygroundStatus("suspended");
+        playground.setPlaygroundStatus("Suspended");
     }
 
     /**
      * @param playground
      */
-    public void deletePlayground(ArrayList<Playground> allPlaygrounds, Playground playground) {
-        allPlaygrounds.remove(playground);
-        playground.getOwner().getPlaygrounds().remove(playground);
-
-        Time currentTime = new Time();
-        currentTime.setCurrentTime();
-        currentTime.setEndingHour(24);
-        currentTime.setEndYear(playground.getAvailableAtTime().getEndYear());
-        currentTime.setEndMonth(playground.getAvailableAtTime().getEndMonth());
-        currentTime.setEndDay(playground.getAvailableAtTime().getEndDay());
-
-        for (int i = 0; i < playground.getBookings().size(); i++) {
-            if (currentTime.conflicts(playground.getBookings().get(i).getTime())) {
-                int bookingDuration = playground.getBookings().get(i).getTime().getDuration();
-                double price = playground.getPrice() * bookingDuration;
-                double currentBalance = playground.getBookings().get(i).getPlayer().getPlayerWallet().getBalance();
-                double ownerBalance = playground.getBookings().get(i).getPlayground().getOwner().getOwnerWallet().getBalance();
-                playground.getBookings().get(i).getPlayground().getOwner().getOwnerWallet().setBalance(ownerBalance - (price * bookingDuration));
-                playground.getBookings().get(i).getPlayer().getPlayerBookings().remove(playground.getBookings().get(i));
-                playground.getBookings().get(i).getPlayer().getPlayerWallet().setBalance(currentBalance + (price * bookingDuration));
-            }
-        }
+    public void deletePlayground(ArrayList<Playground> allPlaygrounds, ArrayList<Playground> unapprovedPlaygrounds, Playground playground) {
+        playground.getOwner().deletePlayground(allPlaygrounds, unapprovedPlaygrounds, playground);
     }
 
     /**
      * @param playground
      */
     public void activatePlayground(Playground playground) {
-        playground.setPlaygroundStatus("activated");
+        playground.setPlaygroundStatus("Activated");
     }
 
 }
