a11731739dc74aad5ccd9b3886edb3295c48b8fe
Rana Habib <60252989+RanaHabib00@users.noreply.github.com>
2020-05-27 15:59:24 +0200

    Ewallet class with JavaDoc

--- System/Ewallet.java
+++ System/Ewallet.java
@@ -1,19 +1,31 @@
 package System;
 
+/**
+ * Ewallet class holds all the important information of the Ewallet such as the balance
+ */
 public class Ewallet {
 
+    /**
+     * the balance of the user
+     */
     private double balance;
 
+    /**
+     * Ewallet constructor initialises the balance
+     */
     public Ewallet() {
-        balance = 0;
+        balance = 100;
     }
 
+    /**
+     * @return user balance
+     */
     public double getBalance() {
         return this.balance;
     }
 
     /**
-     * @param balance
+     * @param balance sets user balance with the balance value
      */
     public void setBalance(double balance) {
         this.balance = balance;
