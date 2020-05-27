ea91084833a8f0e92c7514b651b8c1f5143dc7c5
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:37:52 +0200

    adding javadoc to LateCancellation.java

--- Exceptions/LateCancellation.java
+++ Exceptions/LateCancellation.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * LateCancellation is thrown if player cancels booking after the cancellation period has ended.
+ */
 public class LateCancellation extends Exception {
 
 	public LateCancellation() {
