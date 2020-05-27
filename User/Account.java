91e9b8a6cc1277ba342bbbc8a806eeb8dd688c70
Hesham-Abdelmohsen <55280956+Hesham-Abdelmohsen@users.noreply.github.com>
2020-05-27 17:41:49 +0200

    add java doc to Account.java

--- User/Account.java
+++ User/Account.java
@@ -7,16 +7,44 @@ import System.Verifier;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * account class responsible for holding all the information of users, whether admin, player or playground Owner
+ */
 public abstract class Account {
 
+    /**
+     * the username of the user
+     */
     private String userName;
+    /**
+     * the password of the user
+     */
     private String password;
+    /**
+     * the email of the user
+     */
     private String email;
+    /**
+     * the phone number of the user
+     */
     private String phoneNumber;
+    /**
+     * the static counter to keep track of how many users are there
+     */
     static private int counter = 0;
+    /**
+     * the id of the user
+     */
     private int id;
+    /**
+     * the verifier attribute of the user
+     */
     private Verifier verifier;
 
+    /**
+     * the account constructor, responsible for initialising the attributes of the class
+     * @param verifier used to initialise the attribute of the verifier attribute of the class Account
+     */
     public Account(Verifier verifier) {
         this.userName = "";
         this.password = "";
@@ -27,63 +55,81 @@ public abstract class Account {
         this.verifier = verifier;
     }
 
+    /**
+     * @return getter for usernamt
+     */
     public String getUserName() {
         return this.userName;
     }
 
     /**
-     * @param userName
+     * @param userName setter for username
      */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * @return getter for password
+     */
     public String getPassword() {
         return this.password;
     }
 
     /**
-     * @param password
+     * @param password setter for password
      */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * @return getter for email
+     */
     public String getEmail() {
         return this.email;
     }
 
     /**
-     * @param email
+     * @param email setter for email
      */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * @return getter for phone number
+     */
     public String getPhoneNumber() {
         return this.phoneNumber;
     }
 
     /**
-     * @param phoneNumber
+     * @param phoneNumber setter for phone number
      */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * @return getter for id
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     * @return getter for verifier
+     */
     public Verifier getVerifier() {
         return verifier;
     }
 
     /**
-     * @param username
-     * @param password
-     * @param email
-     * @param phoneNumber
+     * @param username sets current username with username
+     * @param password sets current password with password
+     * @param email sets current email with email
+     * @param phoneNumber sets current phonenumber with phonenumber
      */
     public void register(String username, String password, String email, String phoneNumber) {
         this.userName = username;
@@ -92,6 +138,18 @@ public abstract class Account {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * takes account that needs to be updated
+     * it displays a menu for the user to choose from
+     * the user should choose wether to update email, password or phone number
+     * in case the user is a playground owner, he has the extra option for updating the address of his own
+     * the method checks if the new username, email and address are unique, if not, throws an exception based on what was duplicated
+     * @param accounts account attributes to be updated
+     * @throws InvalidEmail if user entered invalid email
+     * @throws InvalidPassword if user entered invalid password
+     * @throws InvalidNumber if user entered invalid phone number
+     * @throws InvalidAddress if playground owner entered invalid address
+     */
     public void updateProfile(ArrayList<Account> accounts) throws InvalidEmail, InvalidPassword, InvalidNumber, InvalidAddress {
         Scanner input = new Scanner(System.in);
         boolean isOwner = (this instanceof PlaygroundOwner);
