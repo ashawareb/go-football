6c023c8babac671e23ecd59f773935bd88e4a157
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:33:20 +0200

    adding javadoc to InvalidAddress.java

--- Exceptions/InvalidAddress.java
+++ Exceptions/InvalidAddress.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ *  InvalidAddress exception is thrown if program detects duplication in addresses or if it detects that the email is in incorrect format
+ */
 public class InvalidAddress extends Exception {
     public InvalidAddress() {
 
