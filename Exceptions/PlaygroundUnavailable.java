8f32adb1041f2368dacb57f167df532552634798
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:38:21 +0200

    adding javadoc to PlaygroundUnavailable.java

--- Exceptions/PlaygroundUnavailable.java
+++ Exceptions/PlaygroundUnavailable.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * PlaygroundUnavailable is thrown if player tries to book a playground in an unavailable time slot
+ */
 public class PlaygroundUnavailable extends Exception {
 
 	public PlaygroundUnavailable() {
