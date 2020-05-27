603c7dedc02f635dfe362605762a5cdd3518926d
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:28:31 +0200

    update player.java with javadoc

--- User/Player.java
+++ User/Player.java
@@ -12,13 +12,32 @@ import System.Verifier;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Player class responsible for holding all the important methods of the player
+ */
 public class Player extends Account {
+    /**
+     * favourtie team index
+     */
     private int favouriteTeamIndex = -1;
 
+    /**
+     * player wallet
+     */
     private Ewallet playerWallet;
+    /**
+     * player arrayList of teams
+     */
     private ArrayList<Team> teams;
+    /**
+     * player arrayList of bookings
+     */
     private ArrayList<Booking> playerBookings;
 
+    /**
+     * PLayer constructor for initialising the attributes of the player wallet, teams and bookings
+     * @param verifier verifier object from verifier class to call super class of Account
+     */
     public Player(Verifier verifier) {
         super(verifier);
         playerWallet = new Ewallet();
@@ -26,20 +45,29 @@ public class Player extends Account {
         playerBookings = new ArrayList<>();
     }
 
+    /**
+     * @return getter for player wallet
+     */
     public Ewallet getPlayerWallet() {
         return this.playerWallet;
     }
 
+    /**
+     * @return getter for player teams
+     */
     public ArrayList<Team> getTeams() {
         return this.teams;
     }
 
+    /**
+     * @return getter for playerg bookings
+     */
     public ArrayList<Booking> getPlayerBookings() {
         return this.playerBookings;
     }
 
     /**
-     * @param playgrounds
+     * @param playgrounds takes all playgrounds and loops through them all then print one by one
      */
     public void viewPlaygrounds(ArrayList<Playground> playgrounds) {
         for (int i = 0; i < playgrounds.size(); i++) {
@@ -48,8 +76,11 @@ public class Player extends Account {
     }
 
     /**
-     * @param playground
-     * @param criterion
+     * filter playground method is used to filter playground based on the criterion
+     * if the criterion is timeSlot, then it calls the function filterByTimeSlot
+     * if the criterion is location, then it calls the function filterByLocationAndDate
+     * @param playground all the active playgrounds to be filtered
+     * @param criterion the criterion on which the playgrounds will be filtered
      */
     public void filterPlaygrounds(ArrayList<Playground> playground, String criterion) {
 
@@ -70,9 +101,13 @@ public class Player extends Account {
         viewPlaygrounds(filteredPlaygrounds);
     }
 
+
     /**
-     * @param playgrounds
-     * @param time
+     * filterByTimeSlot takes all the playgrounds and then the specific time that the player wants
+     * then checks which of the playgrounds satisfies the time slot and adds it to the filteredPlaygrounds
+     * @param playgrounds all the playgrounds to be filtered
+     * @param time the time slot on which the user wants
+     * @return the filteredPlaygrounds
      */
     public ArrayList<Playground> filterByTimeSlot(ArrayList<Playground> playgrounds, Time time) {
         ArrayList<Playground> filteredPlaygrounds = new ArrayList<>();
@@ -85,6 +120,14 @@ public class Player extends Account {
         return filteredPlaygrounds;
     }
 
+    /**
+     * filterByLocationAndDate takes all the playgrounds and then the specific time and location that the player wants
+     * then checks which of the playgrounds satisfies the time slot and location then adds it to the filteredPlaygrounds
+     * @param playgrounds all the playgrounds to be filtered
+     * @param addressLocation the location to be compared with
+     * @param time the time slot on which the user wants
+     * @return the filteredPlaygrounds
+     */
     public ArrayList<Playground> filterByLocationAndDate(ArrayList<Playground> playgrounds, String addressLocation, Time time) {
         ArrayList<Playground> filteredPlaygrounds = new ArrayList<>();
         for (int i = 0; i < playgrounds.size(); i++) {
@@ -96,8 +139,22 @@ public class Player extends Account {
         return filteredPlaygrounds;
     }
 
+
     /**
-     * @param bookedPlayground
+     * method responsible for booking a playground
+     * it initiates a new time
+     * then requires the player to enter the desired time slot to be booked
+     * the method checks whether the balance is enough for the booking process or not
+     * the method checks if this playground is booked previously or not
+     * the method then checks whether the time slot is within the range of the time that the playground is available
+     * if the time slot is within the available time slot of the playground, the method adds the booked playground to
+     * the user booking arraylist
+     * the method checks if this playground is booked previously or not
+     * otherwise throws an exception
+     * @param player player that wants to book
+     * @param bookedPlayground playground that the user want to book
+     * @throws InsufficientBalance throws exception if balance is insuffiecnt for the booking process
+     * @throws PlaygroundUnavailable throws exception if the playground is already booked before
      */
     public void bookPlayground(Player player, Playground bookedPlayground) throws InsufficientBalance, PlaygroundUnavailable {
         if (bookedPlayground.getPlaygroundStatus().equals("Activated") && bookedPlayground.getAvailability()) {
@@ -144,6 +201,12 @@ public class Player extends Account {
         }
     }
 
+    /**
+     * create team method is responsible for adding a new team to the player attributes
+     * it takes how many players the user wants to add, then loops through them all to take the name and email
+     * then check if valid team members
+     * @param verifier verified used to verify the team member attributes
+     */
     public void createTeam(Verifier verifier) {
         Scanner input = new Scanner(System.in);
         Team tempTeam = new Team();
@@ -216,6 +279,12 @@ public class Player extends Account {
         if (tempTeam.isFavouriteTeam()) favouriteTeamIndex = teams.indexOf(tempTeam);
     }
 
+    /**
+     * send invitation method is responsible for sending invitation to the team and team member
+     * based on what the player chooses to invite
+     * if the player chose to invite the favourite team, then the method calls the sendInvitationMethod to send invitation to all favourite team members
+     * if the player chose to invite only a specific number of members, the player enters how many players and the method then invites these team members
+     */
     public void sendInvitation() {
         Team teamToInvite = pickTeam();
         boolean invitationWasSuccessful = false;
@@ -247,6 +316,10 @@ public class Player extends Account {
         }
     }
 
+    /**
+     * @param email checks if that email exists in the team, if it is, sends an invitation
+     * @return whether or not the email was found
+     */
     public boolean sendEmailInvitation(String email) {
         boolean emailFound = false;
         for (int i = 0; i < teams.size(); i++) {
@@ -258,6 +331,13 @@ public class Player extends Account {
         return emailFound;
     }
 
+    /**
+     * a method for cancelling booking
+     * the method takes a booking as a parameter then compares current time with the booking initial time
+     * if the current time passes the cancellation period, the method displays a message to the player that the
+     * cancellation period was passed and will not refund the player, if not, then the booking is cancelled and the player is refunded.
+     * @param booking booking to be cancelled
+     */
     public void cancelBooking(Booking booking) {
         int cancellationPeriod = booking.getPlayground().getCancelationPeriod();
         Time cancellationTime = new Time();
@@ -279,6 +359,12 @@ public class Player extends Account {
         System.out.println("Booking cancelled");
     }
 
+    /**
+     * a method for modifying team
+     * the method displays a menu asking the player whether he wants to modify name or team members or favourite team
+     * @param modifyTeam team to be modified
+     * @throws InvalidEmail throws exception if email is invalid
+     */
     public void modifyTeam(Team modifyTeam) throws InvalidEmail {
         System.out.println("===========================");
         System.out.println("What would you like to change: ");
@@ -345,6 +431,10 @@ public class Player extends Account {
         }
     }
 
+    /**
+     * displays all teams and requires the player to choose one of them then returns that chosen team
+     * @return the chosen team
+     */
     public Team pickTeam() {
         System.out.println("===========================");
         System.out.println("Your Teams");
@@ -361,6 +451,11 @@ public class Player extends Account {
         return getTeams().get(userChoice - 1);
     }
 
+    /**
+     * takes a specific team, then requires the player to choose one of the team members, then returns that member
+     * @param team the team to be chosen from
+     * @return the chosen team member
+     */
     public Team.TeamMember pickTeamMember(Team team) {
         System.out.println("===========================");
         System.out.println(team.getTeamName() + " team Members: ");
@@ -373,6 +468,9 @@ public class Player extends Account {
         return team.getMembers().get(userChoice - 1);
     }
 
+    /**
+     * @return String containing all the player information such as the name, favourite team, email and phone number
+     */
     @Override
     public String toString() {
         return "Player user name:" + getUserName() + '\n' +
