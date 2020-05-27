36cd64b0d287a9a8b5a4ef848c167007d7e5f373
Hesham-Abdelmohsen <55280956+Hesham-Abdelmohsen@users.noreply.github.com>
2020-05-27 17:46:58 +0200

    adding javadoc to Administrator.java

--- User/Administrator.java
+++ User/Administrator.java
@@ -6,8 +6,15 @@ import System.Verifier;
 
 import java.util.ArrayList;
 
+/**
+ * Administrator class responsible for conducting all the important methods of the administrator
+ */
 public class Administrator extends Account {
 
+    /**
+     * Administrator constructor initialises the username, password, email and phone number of the admin
+     * @param verifier to call the account constructor
+     */
     public Administrator(Verifier verifier) {
         super(verifier);
         setUserName("admin");
@@ -17,7 +24,8 @@ public class Administrator extends Account {
     }
 
     /**
-     * @param playground
+     * approving a specific playground by setting the playground statues to activated
+     * @param playground the playground to be approved
      */
     public void approvePlayground(Playground playground) {
         playground.setAvailability(true);
@@ -25,21 +33,26 @@ public class Administrator extends Account {
     }
 
     /**
-     * @param playground
+     * suspending a specific playground by setting the playground statues to suspended
+     * @param playground the playground to be suspended
      */
     public void suspendPlayground(Playground playground) {
         playground.setPlaygroundStatus("Suspended");
     }
 
     /**
-     * @param playground
+     * deleting a specific playground
+     * @param playground the playground to be deleted
+     * @param allPlaygrounds arraylist of all playgrounds
+     * @param unapprovedPlaygrounds arraylist of all unapproved playgrounds
      */
     public void deletePlayground(ArrayList<Playground> allPlaygrounds, ArrayList<Playground> unapprovedPlaygrounds, Playground playground) {
         playground.getOwner().deletePlayground(allPlaygrounds, unapprovedPlaygrounds, playground);
     }
 
     /**
-     * @param playground
+     * activating a specific playground by setting the playground statues to activated
+     * @param playground the playground to be activated
      */
     public void activatePlayground(Playground playground) {
         playground.setPlaygroundStatus("Activated");
