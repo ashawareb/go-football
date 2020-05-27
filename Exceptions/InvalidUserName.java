7148a78ac1d9963c5b1c54fe2e99cd1798fc2e52
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:37:13 +0200

    adding javadoc to InvalidUserName.java

--- Exceptions/InvalidUserName.java
+++ Exceptions/InvalidUserName.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * InvalidUserName is thrown if program detects duplication in username
+ */
 public class InvalidUserName extends Exception {
 
 	public InvalidUserName() {
