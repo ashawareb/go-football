61a8e175c44d369d84f64f80c971d6304079d5ea
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:32:41 +0200

    adding javadoc to InsufficientBalance.java

--- Exceptions/InsufficientBalance.java
+++ Exceptions/InsufficientBalance.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * InsufficientBalance is thrown when player tries to book a playground with insufficient balance.
+ */
 public class InsufficientBalance extends Exception {
 
 	public InsufficientBalance() {
