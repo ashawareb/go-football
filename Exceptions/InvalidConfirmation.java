ed2943c105b241778acd5bf8fccdbbd1778519a2
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:34:06 +0200

    adding javadoc to InvalidConfirmation.java

--- Exceptions/InvalidConfirmation.java
+++ Exceptions/InvalidConfirmation.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ *  InvalidConfirmation is thrown if user sent invalid confirmation number during registration
+ */
 public class InvalidConfirmation extends Exception {
 
 	public InvalidConfirmation() {
