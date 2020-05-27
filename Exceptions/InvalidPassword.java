f233a38b68a24bcbb2a296f7bfce8a7a8a46c44d
moaz basheer <moazbasheer97@gmail.com>
2020-05-27 16:36:11 +0200

    adding javadoc to InvalidPassword.java

--- Exceptions/InvalidPassword.java
+++ Exceptions/InvalidPassword.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * InvalidPassword is thrown if program detects that the password is too short
+ */
 public class InvalidPassword extends Exception {
 
 	public InvalidPassword() {
