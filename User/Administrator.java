02f5ceb54d0b68a0bcaf5d15b548c525c5feb3ba
Mohannad Ayman <mohannadaly98@gmail.com>
2020-05-28 09:46:38 +0200

    Updated approvePlayground method
    display playground information before approving
    ask for confirmation to approve playground
    change approvePlayground to boolean to indicate operation status

--- User/Administrator.java
+++ User/Administrator.java
@@ -13,6 +13,7 @@ public class Administrator extends Account {
 
     /**
      * Administrator constructor initialises the username, password, email and phone number of the admin
+     *
      * @param verifier to call the account constructor
      */
     public Administrator(Verifier verifier) {
@@ -25,15 +26,31 @@ public class Administrator extends Account {
 
     /**
      * approving a specific playground by setting the playground statues to activated
+     *
      * @param playground the playground to be approved
+     * @return whether or not the playground was approved
      */
-    public void approvePlayground(Playground playground) {
-        playground.setAvailability(true);
-        playground.setPlaygroundStatus("Activated");
+    public boolean approvePlayground(Playground playground) {
+        System.out.println("Playground information:\n" + playground);
+        System.out.println("Would you like to approve this playground?");
+        System.out.println("===========================");
+        System.out.println("[1] Yes");
+        System.out.println("[2] No");
+        System.out.println("===========================");
+        System.out.println("Your Choice: ");
+        int userChoice = getVerifier().getUserChoice(1, 2);
+        if (userChoice == 1) {
+            playground.setAvailability(true);
+            playground.setPlaygroundStatus("Activated");
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * suspending a specific playground by setting the playground statues to suspended
+     *
      * @param playground the playground to be suspended
      */
     public void suspendPlayground(Playground playground) {
@@ -42,8 +59,9 @@ public class Administrator extends Account {
 
     /**
      * deleting a specific playground
-     * @param playground the playground to be deleted
-     * @param allPlaygrounds arraylist of all playgrounds
+     *
+     * @param playground            the playground to be deleted
+     * @param allPlaygrounds        arraylist of all playgrounds
      * @param unapprovedPlaygrounds arraylist of all unapproved playgrounds
      */
     public void deletePlayground(ArrayList<Playground> allPlaygrounds, ArrayList<Playground> unapprovedPlaygrounds, Playground playground) {
@@ -52,6 +70,7 @@ public class Administrator extends Account {
 
     /**
      * activating a specific playground by setting the playground statues to activated
+     *
      * @param playground the playground to be activated
      */
     public void activatePlayground(Playground playground) {
