f0b39070e33ee95a9f1ca1679ec8aa3c54549e51
Mohannad Ayman <mohannadaly98@gmail.com>
2020-05-28 11:16:15 +0200

    use the new getWallet method from Account class when making transactions

--- System/Booking.java
+++ System/Booking.java
@@ -5,65 +5,102 @@ import User.Team;
 
 import java.util.ArrayList;
 
+/**
+ * Booking class responsible for holding the booking information
+ */
 public class Booking {
 
+    /**
+     * playground attribute for holding the playground that was booked
+     */
     private Playground playground;
+    /**
+     * player attribute for holding the player that booked the playground
+     */
     private Player player;
+    /**
+     * time attribute for holding how long the playground is booked
+     */
     private Time time;
+    /**
+     * teamMembers that are to be invited to the booked playground
+     */
     private ArrayList<Team.TeamMember> teamMembers;
 
+    /**
+     * Booking constructor initiates the following:
+     * a new playground
+     * a new player
+     * a new time
+     * @param verifier takes verifier to use it to initialize the playground constructor
+     */
     public Booking(Verifier verifier) {
         playground = new Playground(verifier);
         player = new Player(verifier);
         time = new Time();
     }
 
+    /**
+     * @return booked playground
+     */
     public Playground getPlayground() {
         return this.playground;
     }
- 
+
     /**
-     * @param playground
+     * @param playground sets current booked playground with the value of playground
      */
     public void setPlayground(Playground playground) {
         this.playground = playground;
     }
 
+    /**
+     * @return current player that booked the playground
+     */
     public Player getPlayer() {
         return this.player;
     }
 
     /**
-     * @param player
+     * @param player sets current player that booked the playground
      */
     public void setPlayer(Player player) {
         this.player = player;
     }
 
+    /**
+     * @return how long the playground is booked
+     */
     public Time getTime() {
         return this.time;
     }
 
     /**
-     * @param time
+     * @param time sets the current time range with the value of time
      */
     public void setTime(Time time) {
         this.time = time;
     }
 
+    /**
+     * refunds player with money if the cancellation was within the cancellation period
+     */
     public void refund() {
         Time currentTime = new Time();
         currentTime.setCurrentTime();
         if (currentTime.conflicts(getTime())) {
             int bookingDuration = getTime().getDuration();
             double price = playground.getPrice() * bookingDuration;
-            double currentBalance = getPlayer().getPlayerWallet().getBalance();
-            double ownerBalance = getPlayground().getOwner().getOwnerWallet().getBalance();
-            getPlayground().getOwner().getOwnerWallet().setBalance(ownerBalance - (price * bookingDuration));
-            getPlayer().getPlayerWallet().setBalance(currentBalance + (price * bookingDuration));
+            double currentBalance = getPlayer().getWallet().getBalance();
+            double ownerBalance = getPlayground().getOwner().getWallet().getBalance();
+            getPlayground().getOwner().getWallet().setBalance(ownerBalance - (price * bookingDuration));
+            getPlayer().getWallet().setBalance(currentBalance + (price * bookingDuration));
         }
     }
 
+    /**
+     * @return the information of the booking such as the booking duration and the player
+     */
     @Override
     public String toString() {
         return "Booking duration:\n" + time + '\n' +
